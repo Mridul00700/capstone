5c9ac698c94aca82971b80115a53051d058ab62b
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-11-14 18:29:10 +0530

    chart-Line-Done

--- src/components/IssueTracker/ViewDetails.js
+++ src/components/IssueTracker/ViewDetails.js
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { useState, useEffect } from 'react';
 import * as actions from '../../actions/actions';
 import { Button } from "@material-ui/core";
+import { Line, Bar } from "react-chartjs-2";
 
 const useStyles = makeStyles((theme) => ({
     customBorderRadius: {
@@ -24,7 +25,7 @@ function ViewDetails({ Issues, Trends, editTrend, addTrend }) {
     const [insights, setInsights] = useState(false);
     // console.log(id);
     // const issues = props.Issues;
-    const issue = Issues.filter(issue => issue.id === id)
+    const issue = { ...Issues.filter(issue => issue.id === id)[0] }
 
 
 
@@ -36,15 +37,15 @@ function ViewDetails({ Issues, Trends, editTrend, addTrend }) {
             editTrend(existingTrendObj);
         }
         else {
-            let issue = Issues.filter(issue => issue.id === id)
-            issue = { ...issue[0], views: 1 }
-            console.log(issue);
-            addTrend(issue)
+            let issue1 = Issues.filter(issue => issue.id === id)
+            issue1 = { ...issue1[0], views: 1 }
+            console.log(issue1);
+            addTrend(issue1)
         }
 
     }, []);
 
-    console.log(Trends);
+    console.log(Trends, Trends.map(issue => issue.title), Trends.map(issue => issue.views));
 
 
     const insightHandler = () => {
@@ -56,12 +57,12 @@ function ViewDetails({ Issues, Trends, editTrend, addTrend }) {
         <div style={{ display: "flex", justifyContent: "center" }}>
             <Paper elevation={6} className={classes.customBorderRadius}>
                 <div style={{ display: "grid", justifyContent: "center" }}>
-                    <div>Title : {issue[0].title}</div>
-                    <div>Date Created: {issue[0].dateCreated}</div>
-                    <div>Severity : {issue[0].severity}</div>
-                    <div>Status : {issue[0].status}</div>
-                    <div>Date Resolved : {issue[0].dateResolved}</div>
-                    <div>Description: {issue[0].description}</div>
+                    <div>Title : {issue.title}</div>
+                    <div>Date Created: {issue.dateCreated}</div>
+                    <div>Severity : {issue.severity}</div>
+                    <div>Status : {issue.status}</div>
+                    <div>Date Resolved : {issue.dateResolved}</div>
+                    <div>Description: {issue.description}</div>
                 </div>
             </Paper>
         </div>
@@ -70,7 +71,23 @@ function ViewDetails({ Issues, Trends, editTrend, addTrend }) {
             <Button variant="contained" color="secondary" onClick={insightHandler} >{insights ? 'Hide Insights' : 'Show Insights'}</Button>
         </div>
         <div>
-            {insights && <p>insights charts</p>}
+            {
+                insights && <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", margin: "10%" }} ><Line
+                    data={{
+                        labels: Trends.map(issue => issue.title),
+                        datasets: [
+                            {
+                                label: "Issues-Trends",
+                                data: Trends.map(issue => issue.views),
+                                fill: true,
+                                backgroundColor: "rgba(75,192,192,0.2)",
+                                borderColor: "rgba(75,192,192,1)"
+                            }
+                        ]
+                    }}
+                />
+                </div>
+            }
         </div>
     </>
     )
